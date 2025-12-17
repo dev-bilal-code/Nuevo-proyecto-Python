f6453bbcda3dc3a02a1d0997d88a39bbd2cdafbc
Bilal <fz605332.com>
2025-12-17 23:02:52 +0100

    Los bucles con range

---
+++ Python/loops/03_loop_range.py
@@ -0,0 +1,45 @@
+
+
+# Range es muy interesante
+# Range es  una secuencia de numeros que comienza desde 0 y termina en un numero especificado
+
+nums = range (8) # no me crea una lista
+print(nums)
+
+
+#print("\n range")
+for num in range(7,10): # por defecto es 0, del 7 hasta el 9
+ print(num)
+
+
+
+# range con inicio,fin,paso
+for num in range(0,50,5):
+ print(num)
+
+
+#
+print("\n Range con numeros negativos")
+for num in range(3459,0,-4):
+  print(num)
+
+
+
+for num in range(-5,1):
+ print(num)
+
+print("\n Range con grandes numeros")
+for num in range(10,16, 1):
+    print(num)
+
+
+nums = range(10)
+list_of_nums =list(nums)
+print(list_of_nums)
+
+
+for _ in range(5):
+ print("hacer cinco veces")
+
+
+# EJERCICIOS
